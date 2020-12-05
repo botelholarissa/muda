7eb75b1e00df5f0225693171f662f74fef2ddb52
botelholarissa <conta.larissa@gmail.com>
2020-12-04 21:02:06 -0300

    create plantSchema

---
+++ backend/src/models/plantSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const plantSchema = new Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        auto: true,
+        required: true
+    },
+    sciName: {
+        type: String,
+        required: true
+    },
+    comName: {
+        type: String,
+        required: true
+    },
+    photo: {
+        type: String,
+        required: true
+    },
+    available: {
+        type: Boolean,
+        required: false
+    },
+    owner: {
+        type: Array,
+        required: true
+    }
+});
+
+const plantsCollection = mongoose.model('plant', plantSchema);
+
+module.exports = plantsCollection
