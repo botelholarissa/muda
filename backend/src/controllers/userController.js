dcabfe358b7b0ae45247962a12a6dae83aedeef3
botelholarissa <conta.larissa@gmail.com>
2020-12-04 22:05:03 -0300

    create getById user

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,14 +2,29 @@ const usersCollection = require('../models/userSchema');
 
 const getAll = (req, res) => {
     usersCollection.find((error, users) => {
-        if(error){
+        if(error)
             return res.status(500).send(error);
-        } else {
+        else 
             return res.status(200).send(users);
+    })
+}
+
+const getById = (req, res) => {
+    id = req.params.id;
+
+    usersCollection.findById(id, (error, user) => {
+        if(error)
+            return res.status(500).send(error);
+        else{
+            if(user !== null)
+                return res.status(200).send(user);
+            else
+                return res.status(404).send({ message: "User doesn't exist in database."});
         }
     })
 }
 
 module.exports = {
-    getAll
+    getAll,
+    getById
 }
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const controller = require('../controllers/userController');
 
 router.get('/', controller.getAll);
+router.get('/:id', controller.getById);
+
 
 
 module.exports = router;
