dffb0ddd8cc4f321749e42a9e8eb647f9e8c9394
botelholarissa <conta.larissa@gmail.com>
2020-12-05 00:50:31 -0300

    create addPlant

--- backend/src/controllers/plantsController.js
+++ backend/src/controllers/plantsController.js
@@ -9,9 +9,20 @@ const getAll = (req, res) => {
     })
 }
 
-
+const addPlant = (req, res) => {
+    const plantBody = req.body;
+    const plant = new plantsCollection(plantBody);
+  
+    plant.save((error) => {
+        if(error)
+            return res.status(400).send(error);
+        else
+            return res.status(200).send(plant)
+    })
+ }
 
 module.exports = {
     getAll,
+    addPlant
     
 }
--- backend/src/routes/plantRoutes.js
+++ backend/src/routes/plantRoutes.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/plantsController');
 
 router.get('/', controller.getAll);
 //router.get('/:id', controller.getById);
-//router.post('/create', controller.addPlant);
+router.post('/create', controller.addPlant);
 //router.put('/update', controller.updatePlant);
 //router.delete('/delete', controller.deletePlant);
 
