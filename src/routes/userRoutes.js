2ec06feb00d135259326ea2afa70c5dba03cecd6
botelholarissa <conta.larissa@gmail.com>
2020-12-10 19:18:45 -0300

    fix localização das pastas

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/userController');
+const testeController = require('../controllers/gardenControllers')
 
 router.get('/', controller.getAll);
 router.get('/:id', controller.getById);
 router.post('/create', controller.addUser);
 router.put('/update', controller.updateUser);
 router.delete('/delete', controller.deleteUser);
+
+router.get('/teste/:id', testeController.getTestById);
 
 
 module.exports = router;
